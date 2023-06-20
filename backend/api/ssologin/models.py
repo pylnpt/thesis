d34258695eab2594939fa17974b2250be17da5dd
Péter Molnár <xodax9898@gmail.com>
2023-06-20 21:58:25 +0200

    feat(Django): Implementation of the GCP SSO auth settings.py: Defined  the CORS rules Addition of the CORS Middleware Created the social account providers Addition of the created apps Addition of the installed libraries --- apps.py: Defined the ssoLogin app ---- models.py: Created the GoogleUser model.

---
+++ backend/api/ssologin/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class GoogleUser(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+
